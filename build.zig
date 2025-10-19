fdda3e8b74e16e7c033c75a9e74d50f6d17a9f27
Mohan <mohanavel15@protonmail.com>
2025-10-19 19:25:47 +0530

    make it build on latest zig!

--- build.zig
+++ build.zig
@@ -4,11 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "quark",
+    const exe_mod = b.addModule("quark_exe", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "quark",
+        .root_module = exe_mod,
     });
 
     b.installArtifact(exe);
